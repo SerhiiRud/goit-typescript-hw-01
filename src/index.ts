0bab1064787f2bb3c6978c1c811f0a7d2ad47210
NataliiaRud <nrud1981@gmail.com>
2023-09-04 12:22:29 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 
 if (button && input) {
-    button.addEventListener("click", () => {
-        concatenation(input.value, "Hello!");
-    });
+  button.addEventListener("click", () => {
+    concatenation(input.value, "hello!");
+  });
 }
